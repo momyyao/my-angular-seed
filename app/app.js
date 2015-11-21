fab81c229b0a77116e312db784eeb805fac2584c
Yaohua Wang <katie.wang.ga@gmail.com>
2015-11-21 16:45:08 -0500

    services changes

--- app/app.js
+++ app/app.js
@@ -5,8 +5,9 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
+  'myApp.services',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1_new'});
+  $routeProvider.otherwise({redirectTo: '/services'});
 }]);
--- app/services/services.js
+++ app/services/services.js
@@ -1,3 +1,17 @@
 /**
  * Created by kwang on 11/21/2015.
  */
+'use strict';
+
+angular.module('myApp.services', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/services', {
+    templateUrl: 'services/services.html',
+    controller: 'ServicesCtrl'
+  });
+}])
+
+.controller('ServicesCtrl', [function() {
+
+}]);
